57af75ba38775003a8e8138ccbe9df6c97ce0306
Theodore GARSON-CORBEAUX <theodore.corbeaux@gmail.com>
2018-02-11 17:44:01 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+};
+
+module.exports = App;
